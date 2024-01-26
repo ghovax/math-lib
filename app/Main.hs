251540677e457c9ae3ed1ee32cddbb43510be362
ghovax <giovannigravili653@gmail.com>
2024-01-26 18:09:35 +0100

    Removed parser, now it's AST only

--- app/Main.hs
+++ app/Main.hs
@@ -1,130 +1,135 @@
-import Data.Functor.Identity (Identity)
-import Text.Parsec
-import Text.Parsec.Expr
-import Text.Parsec.String (Parser)
+import qualified System.Exit as Exit
+import Test.HUnit
+
+-- import Data.Complex
 
 data Expr
-  = Num Double
+  = Real Double
+  | Complex Double Double
   | Var String
   | Sum Expr Expr
   | Prod Expr Expr
   | Div Expr Expr
-  deriving (Show)
+  | Func String [Expr]
+  | Neg Expr
+  deriving (Show, Eq)
 
--- Define a helper function to consume trailing whitespace
-lexeme :: Parser a -> Parser a
-lexeme p = p <* spacesLoc
-  where
-    spacesLoc = many (oneOf " \t") -- Consume spaces and tabs
+-- | Compute the derivative of an expression with respect to a variable.
+deriv :: Expr -> String -> Expr
+deriv (Real _) _ = Real 0
+deriv (Complex _ _) _ = Complex 0 0
+deriv (Var v) x
+  | v == x = Real 1
+  | otherwise = Real 0
+deriv (Sum e1 e2) x = Sum (deriv e1 x) (deriv e2 x)
+deriv (Prod e1 e2) x = Sum (Prod (deriv e1 x) e2) (Prod e1 (deriv e2 x))
+deriv (Div e1 e2) x =
+  Div
+    ( Sum
+        (Prod (deriv e1 x) e2)
+        (Neg (Prod e1 (deriv e2 x)))
+    )
+    (Prod e2 e2)
+deriv (Func name args) x = Func name (map (`deriv` x) args)
+deriv (Neg e) x = Neg (deriv e x)
 
--- Compute derivatives
-derivative :: Expr -> String -> Expr
-derivative (Num _) _ = Num 0
-derivative (Var v) x
-  | v == x = Num 1
-  | otherwise = Num 0
-derivative (Sum e1 e2) x = Sum (derivative e1 x) (derivative e2 x)
-derivative (Prod e1 e2) x = Sum (Prod (derivative e1 x) e2) (Prod e1 (derivative e2 x))
-derivative (Div e1 e2) x = Div (Sum (Prod (derivative e1 x) e2) (neg (Prod e1 (derivative e2 x)))) (Prod e2 e2)
+-- | Simplify an expression by applying algebraic simplifications.
+simpl :: Expr -> Expr
+simpl (Real n) = Real n
+simpl (Complex r i) = Complex r i
+simpl (Var v) = Var v
+simpl (Sum e1 e2) =
+  let simplE1 = simpl e1
+      simplE2 = simpl e2
+   in case (simplE1, simplE2) of
+        (Real 0, _) -> simplE2 -- If the first expression is 0, return the second expression
+        (_, Real 0) -> simplE1 -- If the second expression is 0, return the first expression
+        (Real n1, Real n2) -> Real (n1 + n2)
+        (Complex r1 i1, Complex r2 i2) -> Complex (r1 + r2) (i1 + i2)
+        _ -> Sum simplE1 simplE2
+simpl (Prod e1 e2) =
+  let simplE1 = simpl e1
+      simplE2 = simpl e2
+   in case (simplE1, simplE2) of
+        (Real n1, Real n2) -> Real (n1 * n2)
+        (Complex r1 i1, Complex r2 i2) -> Complex (r1 * r2 - i1 * i2) (r1 * i2 + i1 * r2)
+        (Real 0, _) -> Real 0
+        (_, Real 0) -> Real 0
+        (Real 1, e) -> e
+        (e, Real 1) -> e
+        _ -> Prod simplE1 simplE2
+simpl (Div e1 e2) =
+  let simplE1 = simpl e1
+      simplE2 = simpl e2
+   in case (simplE1, simplE2) of
+        (_, Real 0) -> error "Division by zero"
+        (Real 0, _) -> Real 0
+        (e, Real 1) -> e
+        _ -> Div simplE1 simplE2
+simpl (Func name args) = Func name (map simpl args)
+simpl (Neg e) =
+  case simpl e of
+    Real n -> Real (-n)
+    (Complex r i) -> Complex (-r) (-i)
+    s -> Neg s
 
--- Define unary negation
-neg :: Expr -> Expr
-neg = Prod (Num (-1))
+-- | Test cases for the 'deriv' function.
+testDeriv :: Test
+testDeriv =
+  TestList
+    [ TestCase $
+        assertEqual
+          "Derivation of simple expression"
+          (simpl (deriv (Prod (Real 2) (Var "x")) "x"))
+          (Real 2),
+      TestCase $
+        assertEqual
+          "Derivation with respect to different variables"
+          (simpl (deriv (Sum (Var "x") (Var "y")) "y"))
+          (Real 1),
+      TestCase $
+        assertEqual
+          "Derivation of product of two functions"
+          (simpl (deriv (Prod (Func "f" [Var "x"]) (Func "g" [Var "x"])) "x"))
+          ( Sum
+              (Prod (deriv (Func "f" [Var "x"]) "x") (Func "g" [Var "x"]))
+              (Prod (Func "f" [Var "x"]) (deriv (Func "g" [Var "x"]) "x"))
+          ),
+      TestCase $
+        assertEqual
+          "Derivation of complex expression"
+          (simpl (deriv (Prod (Sum (Real 2) (Var "x")) (Div (Var "y") (Var "z"))) "x"))
+          (Div (Var "y") (Var "z"))
+    ]
 
--- Define a helper function to parse integer and floating-point numbers
-number :: Parser Double
-number = lexeme $ read <$> many1 digit <* optional (char '.' >> many digit)
+-- | Test cases for the 'simpl' function.
+testSimpl :: Test
+testSimpl =
+  TestList
+    [ TestCase $
+        assertEqual
+          "Simplify addition"
+          (simpl (Sum (Real 2) (Real 3)))
+          (Real 5),
+      TestCase $
+        assertEqual
+          "Simplify multiplication"
+          (simpl (Prod (Real 2) (Var "x")))
+          (Prod (Real 2) (Var "x")),
+      TestCase $
+        assertEqual
+          "Simplify function"
+          (simpl (Func "f" [Var "x"]))
+          (Func "f" [Var "x"]),
+      TestCase $
+        assertEqual
+          "Simplify complex expression"
+          (simpl (Sum (Prod (Real 3) (Var "x")) (Prod (Sum (Real 2) (Var "y")) (Real 4))))
+          (Sum (Prod (Real 3) (Var "x")) (Prod (Sum (Real 2) (Var "y")) (Real 4)))
+    ]
 
--- Define a helper function to parse variables (e.g., "x", "y")
-variable :: Parser String
-variable = lexeme $ many1 letter
-
--- Define a parser for expressions
-expr :: Parser Expr
-expr = term `chainl1` addOp
-
--- Define a term parser
-term :: Parser Expr
-term = factor `chainl1` mulOp
-
--- Define a factor parser
-factor :: Parser Expr
-factor = lexeme $ parens expr <|> Var <$> variable <|> Num <$> number
-
--- Define addition operator
-addOp :: Parser (Expr -> Expr -> Expr)
-addOp = reservedOp "+" >> return Sum
-
--- Define multiplication and division operators
-mulOp :: Parser (Expr -> Expr -> Expr)
-mulOp =
-  do
-    reservedOp "*"
-    return Prod
-    <|> do
-      reservedOp "/"
-      return Div
-
--- Define a helper function to parse parentheses
-parens :: Parser a -> Parser a
-parens = between (lexeme $ char '(') (lexeme $ char ')')
-
--- Define a helper function to parse reserved operators
-reservedOp :: String -> Parser ()
-reservedOp op = lexeme $ do
-  _ <- string op
-  notFollowedBy (oneOf "+-*/")
-
--- Parse an expression
-parseExpr :: String -> Either ParseError Expr
-parseExpr = parse (lexeme expr) ""
-
--- Pretty print an expression
-prettyPrint :: Expr -> String
-prettyPrint (Num n) = show n
-prettyPrint (Var v) = v
-prettyPrint (Sum e1 e2) = "(" ++ prettyPrint e1 ++ " + " ++ prettyPrint e2 ++ ")"
-prettyPrint (Prod e1 e2) = "(" ++ prettyPrint e1 ++ " * " ++ prettyPrint e2 ++ ")"
-prettyPrint (Div e1 e2) = "(" ++ prettyPrint e1 ++ " / " ++ prettyPrint e2 ++ ")"
-
--- Simplify an expression
-simplify :: Expr -> Expr
-simplify (Num n) = Num n
-simplify (Var v) = Var v
-simplify (Sum e1 e2) =
-  let simplifiedE1 = simplify e1
-      simplifiedE2 = simplify e2
-   in case (simplifiedE1, simplifiedE2) of
-        (Num n1, Num n2) -> Num (n1 + n2)
-        _ -> Sum simplifiedE1 simplifiedE2
-simplify (Prod e1 e2) =
-  let simplifiedE1 = simplify e1
-      simplifiedE2 = simplify e2
-   in case (simplifiedE1, simplifiedE2) of
-        (Num n1, Num n2) -> Num (n1 * n2)
-        (Num 0, _) -> Num 0
-        (_, Num 0) -> Num 0
-        (Num 1, e) -> e
-        (e, Num 1) -> e
-        _ -> Prod simplifiedE1 simplifiedE2
-simplify (Div e1 e2) =
-  let simplifiedE1 = simplify e1
-      simplifiedE2 = simplify e2
-   in case (simplifiedE1, simplifiedE2) of
-        (_, Num 0) -> error "Division by zero"
-        (Num 0, _) -> Num 0
-        (e, Num 1) -> e
-        _ -> Div simplifiedE1 simplifiedE2
-
+-- | Run all test cases.
 main :: IO ()
 main = do
-  let input = "3*x + (2 + y) * 4"
-  case parseExpr input of
-    Left err -> putStrLn $ "Error: " ++ show err
-    Right result -> do
-      putStrLn $ "Parsed: " ++ show result
-      let var = "x"
-      let resultDerivative = derivative result var
-      putStrLn $ "Derivative with respect to " ++ var ++ ": " ++ show resultDerivative
-      let simplifiedDerivative = simplify resultDerivative
-      putStrLn $ "Simplified derivative: " ++ prettyPrint simplifiedDerivative
+  result <- runTestTT (TestList [TestLabel "Simplification tests" testSimpl, TestLabel "Derivation tests" testDeriv])
+  if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
