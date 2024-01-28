22069a7f4a67a1f1214838c9e4b94c1067d6aa23
ghovax <giovannigravili653@gmail.com>
2024-01-28 20:04:55 +0100

    Simplified version

--- app/Main.hs
+++ app/Main.hs
@@ -1,135 +1,95 @@
-import qualified System.Exit as Exit
-import Test.HUnit
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
--- import Data.Complex
+{-# HLINT ignore "Eta reduce" #-}
 
+-- |
+-- Module      : Main
+-- Description : Provides operations for manipulating the Expr data type.
+--
+-- This module defines various operations on the 'Expr' data type, including substitution, simplification, and repeated simplification.
 data Expr
-  = Real Double
-  | Complex Double Double
-  | Var String
-  | Sum Expr Expr
-  | Prod Expr Expr
-  | Div Expr Expr
-  | Func String [Expr]
-  | Neg Expr
+  = -- | Addition operation
+    Sum Expr Expr
+  | -- | Numeric constant
+    Num Double
+  | -- | Negation operation
+    Neg Expr
+  | -- | Multiplication operation
+    Prod Expr Expr
+  | -- | Variable
+    Var String
+  | -- | Exponentiation operation
+    Pow Expr Expr
+  | -- | Inversion operation
+    Inv Expr
   deriving (Show, Eq)
 
--- | Compute the derivative of an expression with respect to a variable.
-deriv :: Expr -> String -> Expr
-deriv (Real _) _ = Real 0
-deriv (Complex _ _) _ = Complex 0 0
-deriv (Var v) x
-  | v == x = Real 1
-  | otherwise = Real 0
-deriv (Sum e1 e2) x = Sum (deriv e1 x) (deriv e2 x)
-deriv (Prod e1 e2) x = Sum (Prod (deriv e1 x) e2) (Prod e1 (deriv e2 x))
-deriv (Div e1 e2) x =
-  Div
-    ( Sum
-        (Prod (deriv e1 x) e2)
-        (Neg (Prod e1 (deriv e2 x)))
-    )
-    (Prod e2 e2)
-deriv (Func name args) x = Func name (map (`deriv` x) args)
-deriv (Neg e) x = Neg (deriv e x)
+-- | Subtracts expression 'b' from 'a'.
+sub :: Expr -> Expr -> Expr
+sub a b = Sum a (Neg b)
 
--- | Simplify an expression by applying algebraic simplifications.
+-- | Left division: Divides expression 'a' by 'b'.
+ldiv :: Expr -> Expr -> Expr
+ldiv a b = Prod a (Inv b)
+
+-- | Right division: Divides expression 'b' by 'a'.
+rdiv :: Expr -> Expr -> Expr
+rdiv a b = Prod (Inv a) b
+
+-- | Replaces occurrences of 'targ' with 'rule' in the expression.
+repl :: Expr -> Expr -> Expr -> Expr
+repl (Sum a b) targ rule = Sum (repl a targ rule) (repl b targ rule)
+repl (Num x) _ _ = Num x
+repl (Neg a) targ rule = Neg (repl a targ rule)
+repl (Prod a b) targ rule = Prod (repl a targ rule) (repl b targ rule)
+repl (Var v) targ rule
+  | Var v == targ = rule
+  | otherwise = Var v
+repl (Pow a b) targ rule = Pow (repl a targ rule) (repl b targ rule)
+repl (Inv a) targ rule = Inv (repl a targ rule)
+
+-- | Replaces all occurrences of target expressions with their corresponding rules.
+replAll :: Expr -> [(Expr, Expr)] -> Expr
+replAll e s = foldl (\a (targ, rule) -> repl a targ rule) e s
+
+-- | Simplifies the expression by evaluating constant subexpressions.
 simpl :: Expr -> Expr
-simpl (Real n) = Real n
-simpl (Complex r i) = Complex r i
-simpl (Var v) = Var v
-simpl (Sum e1 e2) =
-  let simplE1 = simpl e1
-      simplE2 = simpl e2
-   in case (simplE1, simplE2) of
-        (Real 0, _) -> simplE2 -- If the first expression is 0, return the second expression
-        (_, Real 0) -> simplE1 -- If the second expression is 0, return the first expression
-        (Real n1, Real n2) -> Real (n1 + n2)
-        (Complex r1 i1, Complex r2 i2) -> Complex (r1 + r2) (i1 + i2)
-        _ -> Sum simplE1 simplE2
-simpl (Prod e1 e2) =
-  let simplE1 = simpl e1
-      simplE2 = simpl e2
-   in case (simplE1, simplE2) of
-        (Real n1, Real n2) -> Real (n1 * n2)
-        (Complex r1 i1, Complex r2 i2) -> Complex (r1 * r2 - i1 * i2) (r1 * i2 + i1 * r2)
-        (Real 0, _) -> Real 0
-        (_, Real 0) -> Real 0
-        (Real 1, e) -> e
-        (e, Real 1) -> e
-        _ -> Prod simplE1 simplE2
-simpl (Div e1 e2) =
-  let simplE1 = simpl e1
-      simplE2 = simpl e2
-   in case (simplE1, simplE2) of
-        (_, Real 0) -> error "Division by zero"
-        (Real 0, _) -> Real 0
-        (e, Real 1) -> e
-        _ -> Div simplE1 simplE2
-simpl (Func name args) = Func name (map simpl args)
-simpl (Neg e) =
-  case simpl e of
-    Real n -> Real (-n)
-    (Complex r i) -> Complex (-r) (-i)
-    s -> Neg s
+simpl expr = case expr of
+  Sum (Num a) (Num b) -> Num (a + b)
+  Neg (Num a) -> Num (-a)
+  Prod (Num a) (Num b) -> Num (a * b)
+  Sum a b -> Sum (simpl a) (simpl b)
+  Neg a -> Neg (simpl a)
+  Prod a b -> Prod (simpl a) (simpl b)
+  _ -> expr
 
--- | Test cases for the 'deriv' function.
-testDeriv :: Test
-testDeriv =
-  TestList
-    [ TestCase $
-        assertEqual
-          "Derivation of simple expression"
-          (simpl (deriv (Prod (Real 2) (Var "x")) "x"))
-          (Real 2),
-      TestCase $
-        assertEqual
-          "Derivation with respect to different variables"
-          (simpl (deriv (Sum (Var "x") (Var "y")) "y"))
-          (Real 1),
-      TestCase $
-        assertEqual
-          "Derivation of product of two functions"
-          (simpl (deriv (Prod (Func "f" [Var "x"]) (Func "g" [Var "x"])) "x"))
-          ( Sum
-              (Prod (deriv (Func "f" [Var "x"]) "x") (Func "g" [Var "x"]))
-              (Prod (Func "f" [Var "x"]) (deriv (Func "g" [Var "x"]) "x"))
-          ),
-      TestCase $
-        assertEqual
-          "Derivation of complex expression"
-          (simpl (deriv (Prod (Sum (Real 2) (Var "x")) (Div (Var "y") (Var "z"))) "x"))
-          (Div (Var "y") (Var "z"))
-    ]
+-- | Repeatedly applies 'simpl' until the expression no longer changes.
+simplRep :: Expr -> Expr
+simplRep expr = let s = simpl expr in if s == expr then s else simplRep s
 
--- | Test cases for the 'simpl' function.
-testSimpl :: Test
-testSimpl =
-  TestList
-    [ TestCase $
-        assertEqual
-          "Simplify addition"
-          (simpl (Sum (Real 2) (Real 3)))
-          (Real 5),
-      TestCase $
-        assertEqual
-          "Simplify multiplication"
-          (simpl (Prod (Real 2) (Var "x")))
-          (Prod (Real 2) (Var "x")),
-      TestCase $
-        assertEqual
-          "Simplify function"
-          (simpl (Func "f" [Var "x"]))
-          (Func "f" [Var "x"]),
-      TestCase $
-        assertEqual
-          "Simplify complex expression"
-          (simpl (Sum (Prod (Real 3) (Var "x")) (Prod (Sum (Real 2) (Var "y")) (Real 4))))
-          (Sum (Prod (Real 3) (Var "x")) (Prod (Sum (Real 2) (Var "y")) (Real 4)))
-    ]
+-- | Calculates the derivative of the expression with respect to the given variable.
+deriv :: String -> Expr -> Expr
+deriv var expr = case expr of
+  Var v
+    | v == var -> Num 1 -- Derivative of the variable with respect to itself is 1
+    | otherwise -> Num 0 -- Derivative of a constant with respect to any variable is 0
+  Num _ -> Num 0 -- Derivative of a constant with respect to any variable is 0
+  Sum a b -> Sum (deriv var a) (deriv var b)
+  Neg a -> Neg (deriv var a)
+  Prod a b ->
+    Sum (Prod (deriv var a) b) (Prod a (deriv var b))
+  Pow base expo ->
+    Prod expo (Prod (Pow base (sub expo (Num 1))) (deriv var base))
+  Inv a -> Neg (ldiv (deriv var a) (Pow a (Num 2)))
 
--- | Run all test cases.
+-- | Calculates the n-th derivative of the expression with respect to the given variable.
+derivN :: Int -> String -> Expr -> Expr
+derivN 0 _ expr = expr
+derivN n var expr = derivN (n - 1) var (deriv var expr)
+
 main :: IO ()
 main = do
-  result <- runTestTT (TestList [TestLabel "Simplification tests" testSimpl, TestLabel "Derivation tests" testDeriv])
-  if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
+  -- Example usage of 'repl'
+  let expr = Sum (Var "x") (Prod (Num 3) (Neg (Var "y")))
+  let subs = [(Var "x", Num 5), (Var "y", Num 2)]
+  print (simplRep (replAll expr subs))
